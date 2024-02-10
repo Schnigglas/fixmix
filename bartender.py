693b7ddb90897a5846f63f6b65d626b3d6d9f1a5
Niklas <80709057+Schnigglas@users.noreply.github.com>
2024-02-10 13:30:10 +0100

    Add files via upload

---
+++ bartender.py
@@ -0,0 +1,342 @@
+#!/usr/bin/env python
+import time
+import sys
+import RPi.GPIO as GPIO
+import json
+import traceback
+import threading
+
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
+
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+FONT = ImageFont.truetype("FreeMono.ttf", 20)
+
+from menu import MenuItem, Menu, Back, MenuContext, MenuDelegate
+from drinks import drink_list, drink_options
+
+GPIO.setmode(GPIO.BCM)
+
+SCREEN_WIDTH = 128
+SCREEN_HEIGHT = 64
+
+LEFT_BTN_PIN = 13
+LEFT_PIN_BOUNCE = 200
+
+RIGHT_BTN_PIN = 5
+RIGHT_PIN_BOUNCE = 200
+
+OLED_RESET_PIN = 15
+OLED_DC_PIN = 16
+
+FLOW_RATE = 60.0/150.0
+
+# raspberry Pi pin configuration:
+RST = 14
+DC = 15
+SPI_PORT = 0
+SPI_DEVICE = 0
+
+
+class Bartender(MenuDelegate): 
+	def __init__(self):
+		self.running = False
+
+		# set the oled screen height
+		self.screen_width = SCREEN_WIDTH
+		self.screen_height = SCREEN_HEIGHT
+
+		self.btn1Pin = LEFT_BTN_PIN
+		self.btn2Pin = RIGHT_BTN_PIN
+	 
+	 	# configure interrups for buttons
+		GPIO.setup(self.btn1Pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+		GPIO.setup(self.btn2Pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
+
+		# configure screen
+		spi_bus = 0
+		spi_device = 0
+
+		# this lets py-gaugette 'know' what pins to use in order to reset the display
+		self.led = disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000)) # Change rows & cols values depending on your display dimensions.
+		
+		# initialize library
+		self.led.begin()
+
+		# clear display
+		self.led.clear()
+		self.led.display()
+
+
+		# create image buffer
+		self.image = Image.new('1', (self.screen_width, self.screen_height))
+
+		# load custom font
+		self.font = FONT
+
+		# create drawing object
+		self.draw = ImageDraw.Draw(self.image)
+
+		# load the pump configuration from file
+		self.pump_configuration = Bartender.readPumpConfiguration()
+		for pump in self.pump_configuration.keys():
+			GPIO.setup(self.pump_configuration[pump]["pin"], GPIO.OUT, initial=GPIO.HIGH)
+
+		print ("Done initializing")
+
+	@staticmethod
+	def readPumpConfiguration():
+		return json.load(open('pump_config.json'))
+
+	@staticmethod
+	def writePumpConfiguration(configuration):
+		with open("pump_config.json", "w") as jsonFile:
+			json.dump(configuration, jsonFile)
+
+	def startInterrupts(self):
+		GPIO.add_event_detect(self.btn1Pin, GPIO.FALLING, callback=self.left_btn, bouncetime=LEFT_PIN_BOUNCE)  
+		GPIO.add_event_detect(self.btn2Pin, GPIO.FALLING, callback=self.right_btn, bouncetime=RIGHT_PIN_BOUNCE)  
+
+	def buildMenu(self, drink_list, drink_options):
+		# create a new main menu
+		m = Menu("Main Menu")
+
+		# add drink options
+		drink_opts = []
+		for d in drink_list:
+			drink_opts.append(MenuItem('drink', d["name"], {"ingredients": d["ingredients"]}))
+
+		configuration_menu = Menu("Configure")
+
+		# add pump configuration options
+		pump_opts = []
+		for p in sorted(self.pump_configuration.keys()):
+			config = Menu(self.pump_configuration[p]["name"])
+			# add fluid options for each pump
+			for opt in drink_options:
+				# star the selected option
+				selected = "*" if opt["value"] == self.pump_configuration[p]["value"] else ""
+				config.addOption(MenuItem('pump_selection', opt["name"], {"key": p, "value": opt["value"], "name": opt["name"]}))
+			# add a back button so the user can return without modifying
+			config.addOption(Back("Back"))
+			config.setParent(configuration_menu)
+			pump_opts.append(config)
+
+		# add pump menus to the configuration menu
+		configuration_menu.addOptions(pump_opts)
+		
+		# add a back button to the configuration menu
+		
+		configuration_menu.addOption(Back("Back"))
+		
+		# adds an option that cleans all pumps to the configuration menu
+		configuration_menu.addOption(MenuItem('clean', 'Clean'))
+		configuration_menu.setParent(m)
+
+		m.addOptions(drink_opts)
+		m.addOption(configuration_menu)
+		
+		# create a menu context
+		self.menuContext = MenuContext(m, self)
+
+	def filterDrinks(self, menu):
+		for i in menu.options:
+			if (i.type == "drink"):
+				i.visible = False
+				ingredients = i.attributes["ingredients"]
+				presentIng = 0
+				for ing in ingredients.keys():
+					for p in self.pump_configuration.keys():
+						if (ing == self.pump_configuration[p]["value"]):
+							presentIng += 1
+				if (presentIng == len(ingredients.keys())): 
+					i.visible = True
+			elif (i.type == "menu"):
+				self.filterDrinks(i)
+
+	def selectConfigurations(self, menu):
+		for i in menu.options:
+			if (i.type == "pump_selection"):
+				key = i.attributes["key"]
+				if (self.pump_configuration[key]["value"] == i.attributes["value"]):
+					i.name = "%s %s" % (i.attributes["name"], "*")
+				else:
+					i.name = i.attributes["name"]
+			elif (i.type == "menu"):
+				self.selectConfigurations(i)
+
+	def prepareForRender(self, menu):
+		self.filterDrinks(menu)
+		self.selectConfigurations(menu)
+		return True
+
+	def menuItemClicked(self, menuItem):
+		if (menuItem.type == "drink"):
+			self.makeDrink(menuItem.name, menuItem.attributes["ingredients"])
+			return True
+		elif(menuItem.type == "pump_selection"):
+			self.pump_configuration[menuItem.attributes["key"]]["value"] = menuItem.attributes["value"]
+			Bartender.writePumpConfiguration(self.pump_configuration)
+			return True
+		elif(menuItem.type == "clean"):
+			self.clean()
+			return True
+		return False
+
+	def clean(self):
+		waitTime = 20
+		pumpThreads = []
+
+		self.running = True
+
+		for pump in self.pump_configuration.keys():
+			pump_t = threading.Thread(target=self.pour, args=(self.pump_configuration[pump]["pin"], waitTime))
+			pumpThreads.append(pump_t)
+
+		# start the pump threads
+		for thread in pumpThreads:
+			thread.start()
+
+		# start the progress bar
+		self.progressBar(waitTime)
+
+		# wait for threads to finish
+		for thread in pumpThreads:
+			thread.join()
+
+		# show the main menu
+		self.menuContext.showMenu()
+
+		# sleep for a couple seconds to make sure the interrupts don't get triggered
+		time.sleep(2)
+
+	def displayMenuItem(self, menuItem):
+		print (menuItem.name)
+		self.led.clear()
+		self.draw.rectangle((0,0,self.screen_width,self.screen_height), outline=0, fill=0)
+		self.draw.text((0,20),str(menuItem.name), font=self.font, fill=255)
+		self.led.image(self.image)
+		self.led.display()
+
+	def pour(self, pin, waitTime):
+		GPIO.output(pin, GPIO.LOW)
+		time.sleep(waitTime)
+		GPIO.output(pin, GPIO.HIGH)
+
+	def progressBar(self, waitTime):
+		interval = waitTime / 100.0
+		for x in range(1, 101):
+			self.led.clear()
+			self.draw.rectangle((0,0,self.screen_width,self.screen_height), outline=0, fill=0)
+			self.updateProgressBar(x, y=35)
+			self.led.image(self.image)
+			self.led.display()
+			time.sleep(interval)
+
+	def makeDrink(self, drink, ingredients):
+		# cancel any button presses while the drink is being made
+		# self.stopInterrupts()
+		self.running = True
+
+		# Parse the drink ingredients and spawn threads for pumps
+		maxTime = 0
+		pumpThreads = []
+		for ing in ingredients.keys():
+			for pump in self.pump_configuration.keys():
+				if ing == self.pump_configuration[pump]["value"]:
+					waitTime = ingredients[ing] * FLOW_RATE
+					if (waitTime > maxTime):
+						maxTime = waitTime
+					pump_t = threading.Thread(target=self.pour, args=(self.pump_configuration[pump]["pin"], waitTime))
+					pumpThreads.append(pump_t)
+
+		# start the pump threads
+		for thread in pumpThreads:
+			thread.start()
+
+		# start the progress bar
+		self.progressBar(maxTime)
+
+		# wait for threads to finish
+		for thread in pumpThreads:
+			thread.join()
+
+		# show the main menu
+		self.menuContext.showMenu()
+
+		# sleep for a couple seconds to make sure the interrupts don't get triggered
+		time.sleep(2);
+
+		# reenable interrupts
+		# self.startInterrupts()
+		self.running = False
+
+	def left_btn(self, ctx):
+		print("LEFT_BTN pressed")
+		if not self.running:
+			self.running = True
+			self.menuContext.advance()
+			print("Finished processing button press")
+		self.running = False
+
+	def right_btn(self, ctx):
+		print("RIGHT_BTN pressed")
+		if not self.running:
+			self.running = True
+			self.menuContext.select()
+			print("Finished processing button press")
+			self.running = 2
+			print("Starting button timeout")
+
+	def updateProgressBar(self, percent, x=15, y=15):
+		height = 10
+		width = self.screen_width-2*x
+		for w in range(0, width):
+			self.draw.point((w + x, y), fill=255)
+			self.draw.point((w + x, y + height), fill=255)
+		for h in range(0, height):
+			self.draw.point((x, h + y), fill=255)
+			self.draw.point((self.screen_width-x, h + y), fill=255)
+			for p in range(0, percent):
+				p_loc = int(p/100.0*width)
+				self.draw.point((x + p_loc, h + y), fill=255)
+
+	def run(self):
+		self.startInterrupts()
+		# main loop
+		try:
+
+			try: 
+
+				while True:
+					letter = input(">")
+					if letter == "l":
+						self.left_btn(False)
+					if letter == "r":
+						self.right_btn(False)
+
+			except EOFError:
+				while True:
+					time.sleep(0.1)
+					if self.running not in (True,False):
+						self.running -= 0.1
+						if self.running == 0:
+							self.running = False
+							print("Finished button timeout")
+		  
+		except KeyboardInterrupt:  
+			GPIO.cleanup()       # clean up GPIO on CTRL+C exit  
+		GPIO.cleanup()           # clean up GPIO on normal exit 
+
+		traceback.print_exc()
+
+
+bartender = Bartender()
+bartender.buildMenu(drink_list, drink_options)
+bartender.run()
+
+
+
+
---
+++ drinks.py
@@ -0,0 +1,135 @@
+# drinks.py
+drink_list = [
+	{
+		"name": "Cuba Libre",
+		"ingredients": {
+			"rum": 50,
+			"coke": 150
+		}
+	}, {
+		"name": "Gin Tonic",
+		"ingredients": {
+			"gin": 50,
+			"tonic": 150
+		}
+	}, {
+		"name": "Long Island",
+		"ingredients": {
+			"gin": 15,
+			"rum": 15,
+			"vodka": 15,
+			"tequila": 15,
+			"coke": 100,
+			"oj": 30
+		}
+	}, {
+		"name": "Vodka O",
+		"ingredients": {
+			"vodka": 50,
+			"oj": 150
+		}
+	}, {
+		"name": "Margarita",
+		"ingredients": {
+			"tequila": 50,
+			"mmix": 150
+		}
+	}, {
+		"name": "Gin & Juice",
+		"ingredients": {
+			"gin": 50,
+			"oj": 150
+		}
+	}, {
+		"name": "Tequila Sunrise",
+		"ingredients": {
+			"tequila": 50,
+			"oj": 150
+		}
+	},
+		{
+		"name": "Vodka E",
+		"ingredients": {
+			"vodka": 50,
+			"energy": 230
+			}
+		},
+        {
+		"name": "Pina Colada",
+		"ingredients": {
+			"rum": 25,
+			"cm": 25,
+            "pineapple": 150
+		}
+	},
+         {
+		"name": "Moscow Mule",
+		"ingredients": {
+			"vodka": 50,
+			"gingerbeer": 150
+		}
+	},
+         {
+		"name": "Mimosa",
+		"ingredients": {
+			"champagne": 50,
+			"oj": 150
+		}
+	},
+         {
+		"name": "Blue Lagoon",
+		"ingredients": {
+			"vodka": 35,
+            "bluecuracao": 15, 
+			"oj": 150
+		}
+	},
+        {
+		"name": "Mojito",
+		"ingredients": {
+			"rum": 45,
+            "lime": 25, 
+			"soda": 130
+		}
+	},
+         {
+		"name": "Daiquiri",
+		"ingredients": {
+			"rum": 45,
+            "lime": 25, 
+		}
+	},
+             {
+		"name": "Greyhound",
+		"ingredients": {
+			"vodka": 50,
+            "grapefruit": 150, 
+		}
+	},
+	        {
+		"name": "Energy",
+		"ingredients": {
+			"energy": 200,
+		}
+	}
+]
+
+drink_options = [
+	{"name": "Gin", "value": "gin"},
+	{"name": "Rum", "value": "rum"},
+	{"name": "Vodka", "value": "vodka"},
+	{"name": "Tequila", "value": "tequila"},
+	{"name": "Tonic Water", "value": "tonic"},
+	{"name": "Cola", "value": "coke"},
+	{"name": "O-Saft", "value": "oj"},
+	{"name": "Margarita Mix", "value": "mmix"},
+    {"name": "Energy", "value": "energy"},
+    {"name": "Kokusnussmilch", "value": "cm"},
+    {"name": "Ananassaft", "value": "pineapple"},
+    {"name": "Ginger Beer", "value": "gingerbeer"},
+    {"name": "Champagner", "value": "champagne"},
+    {"name": "Blue Curacao", "value": "bluecuracao"},
+    {"name": "Limettensaft", "value": "lime"},
+    {"name": "Soda", "value": "soda"},
+    {"name": "Grapefruitsaft", "value": "grapefruit"}  
+]
